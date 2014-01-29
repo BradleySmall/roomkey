4495defaf640fa7d0db13121c4136e1159881375
Bradley M. Small <bradley_small@hotmail.com>
2014-01-29 15:26:41 -0500

    fix underscores vs dashes

--- src/roomkey/core.clj
+++ src/roomkey/core.clj
@@ -6,18 +6,18 @@
   "URL encode a string"
   [string]
   (URLEncoder/encode string))
-  
-(defn search_twit_api
+
+(defn search-twit-api
   "build the twitter search string from urlencoded string"
   [string]
   (str "https://api.twitter.com/1.1/search/tweets.json?q=" (encode string)  "&src=typd"))
-  ;(str "https://api.twitter.com/1.1/search/tweets.json?q=" (encode string)  "&src=typd&access_token=AAAA%2FAAA%3DAAAAAAAA"))
+;(str "https://api.twitter.com/1.1/search/tweets.json?q=" (encode string)  "&src=typd&access_token=AAAA%2FAAA%3DAAAAAAAA"))
 
 (defn -main
   "prints the twitter api search results"
   [& args]
-  (let [url_string (search_twit_api (first args))]
-    (println url_string)
+  (let [url-string (search-twit-api (first args))]
+    (println url-string)
 
-    (println (apply str (:body (client/get url_string)))))
-)
+    ;(println (apply str (:body (client/get url-string))))
+    ))
